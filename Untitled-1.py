583c5fbfc8a7de6aee9b652080459b6602b471a5
qjrdb55 <69147401+qjrdb55@users.noreply.github.com>
2021-06-29 00:07:49 +0900

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,35 @@
+import discord, asyncio, datetime, pytz
+
+client = discord.Client()
+
+@client.event
+async def on_ready(): # 봇이 실행되면 한 번 실행됨
+    print("이 문장은 Python의 내장 함수를 출력하는 터미널에서 실행됩니다\n지금 보이는 것 처럼 말이죠")
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("주인님께 조련당하는 중"))
+
+@client.event
+async def on_message(message):
+    if message.content == "테스트": # 메세지 감지
+        await message.channel.send ("{} | {}, Hello".format(message.author, message.author.mention))
+        await message.author.send ("{} | {}, User, Hello".format(message.author, message.author.mention))
+
+    if message.content == "특정입력":
+        ch = client.get_channel(859072254959812648)
+        await ch.send ("{} | {}, User, Hello".format(ch.author, ch.author.mention))
+
+    if message.content.startswith ("!공지"):
+        await message.channel.purge(limit=1)
+        i = (message.author.guild_permissions.administrator)
+        if i is True:
+            notice = message.content[4:]
+            channel = client.get_channel(859075763403227136)
+            embed = discord.Embed(title="**ANNOUNCEMENT**", description="\n\n\n{}\n\n--------------------------------------------------------------".format(notice),timestamp=datetime.datetime.now(pytz.timezone('UTC')), color=0x00ff00)
+            embed.set_footer(text="Powered by Hyerin Chae | 담당 관리자 : jeremyyy#1234".format(message.author), icon_url="https://cdn.discordapp.com/attachments/855365831265222666/858686703509438474/unknown.png")
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/855365831265222666/858687900409397268/unknown.png")
+            await channel.send ("@everyone", embed=embed)
+            await message.author.send("*[ BOT 자동 알림 ]* | 정상적으로 공지가 채널에 작성이 완료되었습니다 : )\n\n[ 기본 작성 설정 채널 ] : {}\n[ 공지 발신자 ] : {}\n\n[ 내용 ]\n{}".format(channel, message.author, notice))
+ 
+        if i is False:
+            await message.channel.send("{}, 당신은 관리자가 아닙니다".format(message.author.mention))
+# 봇을 실행시키기 위한 토큰을 작성해주는 곳
+client.run('ODU4Njc1MjM2MTM1ODk1MDcz.YNhloQ.1IFq_60bUS4nE42iwEbO7I1PXMI')
